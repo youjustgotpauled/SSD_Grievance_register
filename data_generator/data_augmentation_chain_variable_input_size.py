ac4bc53ba94c3b3bce38e5f9258ead1d43533e6c
youjustgotpauled <55904704+youjustgotpauled@users.noreply.github.com>
2020-06-09 01:25:32 +0530

    Update data_augmentation_chain_variable_input_size.py

--- data_generator/data_augmentation_chain_variable_input_size.py
+++ data_generator/data_augmentation_chain_variable_input_size.py
@@ -3,19 +3,6 @@ A data augmentation pipeline suitable for variable-size images that produces eff
 that are similar (but not identical) to those of the original SSD data augmentation
 pipeline while being faster.
 
-Copyright (C) 2018 Pierluigi Ferrari
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 
 from __future__ import division
