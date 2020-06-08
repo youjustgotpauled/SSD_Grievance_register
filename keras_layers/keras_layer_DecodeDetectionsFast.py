2b641af54ee93fd9ac241f5d1835053053f36d5f
youjustgotpauled <55904704+youjustgotpauled@users.noreply.github.com>
2020-06-09 01:31:00 +0530

    Update keras_layer_DecodeDetectionsFast.py

--- keras_layers/keras_layer_DecodeDetectionsFast.py
+++ keras_layers/keras_layer_DecodeDetectionsFast.py
@@ -4,19 +4,6 @@ and more efficient version of the `DetectionOutput` layer type in the original C
 implementation of SSD. For a faithful replication of the original layer, please
 refer to the `DecodeDetections` layer.
 
-Copyright (C) 2018 Pierluigi Ferrari
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 '''
 
 from __future__ import division
